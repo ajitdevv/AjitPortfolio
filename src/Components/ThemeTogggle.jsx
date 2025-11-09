b49ee7b3762ac77f345b63331c177f81d3fb4b3d
ajitdevv <ajitdev014@gmail.com>
2025-11-09 23:40:18 +0530

    toggle button style

--- src/Components/ThemeTogggle.jsx
+++ src/Components/ThemeTogggle.jsx
@@ -2,6 +2,14 @@ import { Moon, Sun } from "lucide-react";
 import { useState } from "react";
 function ThemeToggle() {
   const [isdarkmode, setisdarkmode] = useState(true);
-  return <button> {isdarkmode ? <Sun /> : <Moon />}</button>;
+  return (
+    <button>
+      {isdarkmode ? (
+        <Sun className="h-6 w-6 text-yellow-300" />
+      ) : (
+        <Moon className="h-6 w-6 text-blue-300" />
+      )}
+    </button>
+  );
 }
 export default ThemeToggle;

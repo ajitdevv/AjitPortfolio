514b80a727d815d1a1d84687a63fd5d2d2cf3872
ajitdevv <ajitdev014@gmail.com>
2025-11-11 15:43:21 +0530

    create destop navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import logo from "./reminihi.png";
+import { useEffect, useState } from "react";
 function Navbar() {
   const navitems = [
     { name: "Home", href: "#home" },
@@ -6,11 +7,23 @@ function Navbar() {
     { name: "Projects", href: "#projects" },
     { name: "Contact", href: "#contact" },
   ];
+
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    const checkTheme = () => {
+      const hasDark = document.documentElement.classList.contains("dark");
+      setIsDark(hasDark);
+    };
+    checkTheme();
+    const observer = new MutationObserver(checkTheme);
+    observer.observe(document.documentElement, { attributes: true });
+    return () => observer.disconnect();
+  }, []);
   return (
     <nav>
-      <div className="w-full flex justify-between items-center top-4 right-4 z-50 p-2">
-        <img src={logo} alt="Logo" className="size-7" />
-
+      <div className="w-full hidden justify-around items-center mt-6 mb-4 sm:flex">
+        <img src={isDark ? {logo} : "./reminihi-light.png"} alt="not" />
         <ul className="flex justify-center space-x-4">
           {navitems.map((item) => (
             <li key={item.name}>

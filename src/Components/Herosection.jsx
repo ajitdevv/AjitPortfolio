59807bdbbd83daf5ae48005e1c26a703bbe3e1f9
ajitdevv <ajitdev014@gmail.com>
2025-11-11 23:57:44 +0530

    work on home page

--- src/Components/Herosection.jsx
+++ src/Components/Herosection.jsx
@@ -1,8 +1,9 @@
  function Herosection (){
     return (
-   <>
-   <h1 id="home"><span className="text-primary">Hi,I'm</span><span className="text-primary-foreground">Ajit</span><span className="text-primary">Dev</span></h1>
-   </>
+   <section id="home" className="min-h-screen flex flex-col items-center justify-center">
+   <h1 className="text-5xl font-bold" ><span className="text-primary">Hi,I'm</span><span className="text-primary-foreground">Ajit</span><span className="text-primary">Dev</span></h1>
+   <p className="w-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sit quod ullam alias necessitatibus tempora porro maxime aspernatur accusantium quos aperiam nihil architecto ipsum dicta culpa tenetur, minima, dolorum nulla.</p>
+   </section>
     );
  }
  export default Herosection;

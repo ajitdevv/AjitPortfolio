1856855058afb7a47c1795dc2599780d12f5a706
ajitdevv <ajitdev014@gmail.com>
2025-11-12 15:44:27 +0530

    work on home page

--- src/Components/Herosection.jsx
+++ src/Components/Herosection.jsx
@@ -5,24 +5,13 @@ function Herosection() {
       className="min-h-screen flex flex-col items-center justify-center"
     >
       <h1 className="text-5xl font-bold">
-        <span className="text-primary">Hi,I'm</span>
-        <span className="text-primary-foreground">Ajit</span>
+        <span className="text-primary">Hi,I'm </span>
+        <span className="text-primary-foreground">Ajit </span>
         <span className="text-primary">Dev</span>
       </h1>
-      <p className="w-200">
+      <p className="text-primary w-200 gap-t-6 text-s text-center mt-4 max-w-xl">
         passionate Web Developer blending code and creativity to build modern,
         responsive, and impactful digital experiences."
-        <span>
-          Whether <span> you need</span> a business website, a startup platform,
-          a personal portfolio, an e-commerce store, or a hotel website — I
-          specialize in designing impactful digital solutions that help
-          businesses grow through clean design, fast performance, and
-          user-friendly experiences.
-        </span>
-        <span>
-          I craft modern digital experiences that transform{" "}
-          <span> your vision into reality</span>.
-        </span>
       </p>
     </section>
   );

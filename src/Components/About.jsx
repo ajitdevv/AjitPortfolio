2f7f979942bc72a79c0c4cb324dd51ec64840d4a
ajitdevv <ajitdev014@gmail.com>
2025-11-13 20:40:27 +0530

    Add animation in about section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,12 +1,29 @@
+import logo from "./reminihi.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 function About() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
-    <section id="about">
-      <div className="text-cover p-8 h-full text-center">
-        <h1 className="text-primary bold text-5xl font-bold flex justify-start">
+    <section
+      id="about"
+      className=" flex flex-row justify-around w-full items-center pt-20"
+    >
+      <div
+        data-aos="fade-right" data-aos-duration="3000" 
+        className="about-float text-cover p-8 h-full w-[50%] text-center"
+      >
+        <h1
+          data-aos="fade-right"
+          data-aos-duration="1000"
+          className="text-primary bold text-5xl font-bold flex justify-start"
+        >
           About&nbsp;<span className="text-primary-foreground"> Me</span>
         </h1>
         <h2
-          className="text-primary text-3xl mt-6 mx-auto max-w-4xl"
+          className="text-primary text-3xl mt-6 mx-auto max-w-auto"
           style={{ letterSpacing: "0.05rem" }}
         >
           "I’m
@@ -22,7 +39,7 @@ function About() {
           modern, responsive, and impactful digital experiences."
         </h2>
         <p
-          className="text-primary text-lg mt-6 max-w-4xl mx-auto"
+          className="text-primary text-lg mt-6 max-w-auto mx-auto"
           style={{ letterSpacing: "0.05rem" }}
         >
           Whether you need a business website, a startup platform, a personal
@@ -30,14 +47,21 @@ function About() {
           designing impactful digital solutions that help businesses grow
           through clean design, fast performance, and user-friendly experiences.
         </p>
-        <h3 className="text-primary text-2xl mt-6 max-w-4xl mx-auto">
+        <h3 className="text-primary text-2xl mt-6 max-w-auto mx-auto">
           I craft modern digital experiences that transform your vision into
           reality.
         </h3>
-        <div>
-          <a className="cosmic-button" href="#contact"><button>Contact me</button></a>
-          <a className="cosmic-button" href=""><button>Download CV</button></a>
+        <div className="mt-10 flex flex-row justify-around">
+          <a className="cosmic-button text-lg" href="#contact">
+            <button>Contact me</button>
+          </a>
+          <a className="cosmic-button text-lg" href="">
+            <button>Download CV</button>
+          </a>
         </div>
+      </div>
+      <div data-aos="fade-left" className="w-[30%]">
+        <img className="flex items-center justify-center" src={logo} alt="" />
       </div>
     </section>
   );

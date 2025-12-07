5d6affd4e5e3583bddb28bd2c4ec44a54514f755
ajitdevv <ajitdev014@gmail.com>
2025-12-07 11:44:28 +0530

    commit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,7 +6,7 @@ import { Link, useNavigate } from "react-router-dom";
 function About() {
   const navigate = useNavigate();
   useEffect(() => {
-    AOS.init();
+    // AOS.init();
   }, []);
   return (
     <section
